21c3ac98cfd67aea8d2281d040418420d7318c69
Medalisekri <147340866+Medalisekri@users.noreply.github.com>
2023-10-08 21:24:04 +0100

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,7 @@
+public class main{
+  public static void main (string[] ergs){
+    system.out.printf("Hello and Welcome");
+    for (int i =1; i<=5; i++){
+      system.out.println("i=" +i);
+    }
+  }
