cd2968b936db2170b0743f3ba6f21881a85f155f
Medalisekri <147340866+Medalisekri@users.noreply.github.com>
2023-10-08 21:49:07 +0100

    Create Livre.java

---
+++ Livre.java
@@ -0,0 +1,12 @@
+public class livre{
+private string titre;
+  string auteur;
+  string id;
+  float prix;
+  static int count;
+  public livre(string titre,string auteur,string id,float prix);
+
+  
+  
+  
+  
