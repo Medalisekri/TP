868def2f61e69a9aa1c3ab0d7fca453a80f49853
Medalisekri <147340866+Medalisekri@users.noreply.github.com>
2023-10-09 18:44:41 +0100

    Update Livre.java

--- Livre.java
+++ Livre.java
@@ -1,11 +1,28 @@
-public class livre{
-private string titre;
-  string auteur;
-  string id;
-  float prix;
-  static int count;
-  public livre(string titre,string auteur,string id,float prix);
+public class livre {
+    private int id;
+    private String prenom;
+    private String nom;
+    private double prix;
+    public static int count;
+    public livre (int i,String p, String n ,double pr){
+           this.id=i;
+           this.prenom=p;
+           this.nom=n;
+           this.prix=pr;
+           count++;
 
+
+
+
+}
+public livre () {
+        count++;
+}
+
+    public void affiche() {
+        System.out.println("id "+this.id+" nom "+this.nom+" prenom "+this.prenom+" prix "+this.prix);
+    }
+}
   
   
   
